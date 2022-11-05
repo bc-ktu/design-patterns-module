e38d65540ba4045542f295c25d55c7d8df2d3fed
otijoc77 <otilijajociute@gmail.com>
2022-11-05 17:52:32 +0200

    Integration start, not yet working

---
+++ Testing/IntegrationTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using Moq;
+using Server.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class IntegrationTest
+    {
+        [Test]
+        public async Task IntegrationTest_CheckIfConnected()
+        {
+            bool sendCalled = false;
+            var hub = new GameHub();
+            var mockClients = new Mock<IHubCallerConnectionContext<dynamic>>();
+            hub.Clients = (Microsoft.AspNetCore.SignalR.IHubCallerClients)mockClients.Object;
+            dynamic all = new ExpandoObject();
+            all.broadcastMessage = new Action<string, string>((name, message) => {
+                sendCalled = true;
+            });
+            mockClients.Setup(m => m.All).Returns((ExpandoObject)all);
+            hub.SendMessage("TestUser", "TestMessage");
+            Assert.True(sendCalled);
+        }
+    }
+}
