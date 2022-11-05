f9dfb81dec25e2491f21475a12c8d1215a5d4fef
otijoc77 <otilijajociute@gmail.com>
2022-11-05 15:25:24 +0200

    minor update

---
+++ Testing/MockTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.GameLogic;
+using Utils.GameObjects.Animates;
+using Moq;
+using Utils.Map;
+using Utils.GameObjects;
+
+namespace Testing
+{
+    public class MockTests
+    {
+        private Mock<MapTile> mockTile;
+        private GameMap map;
+        private Player player;
+        private GameObject[] gameObjects;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            mockTile = new Mock<MapTile>();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            GameLogic.ApplyEffects(player, map, gameObjects);
+            Assert.Pass();
+        }
+    }
+}
